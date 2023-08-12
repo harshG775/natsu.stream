015c27ca64fbc92bba73bb314d32781f2f5fc70a
Harsh <hgaur491@gmail.com>
2023-08-12 13:12:32 +0530

    starting home page stroctur

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import { useState ,useEffect} from "react";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+// import { useState ,useEffect} from "react";
+import { BrowserRouter, Routes, Route, Link} from "react-router-dom";
 import "./App.css";
 
-import { TMDB } from "../modules/fetching";
+// import { TMDB } from "../modules/fetching";
 // import CardItem from "./components/cardItem/CardItem";
 export default function App() {
-    return <PageRoutes />;
+    return (
+        <>
+        <PageRoutes />
+        </>
+    );
 }
 
 // navbar
@@ -27,31 +31,22 @@ function NavBar() {
 function PageRoutes() {
     return (
         <BrowserRouter>
-            <NavBar />
+                <NavBar />
             <Routes>
-                <Route path='/' element={<Welcome />} />
-                <Route path='/home' element={<Home />} />
-                {/* <Route path='/movies' element={<Movies />} />
-                <Route path='/tvshow' element={<TvShow />} /> */}
+                <Route path='/' element={<HomePage />} />
+                
+                <Route path='/home' element={<HomePage />} />
+                <Route path='/movies' element={<MoviesPage />} />
+                <Route path='/tvshow' element={<TvShowPage />} />
+                <Route path='/search' element={<SearchPage />} />
             </Routes>
         </BrowserRouter>
     );
 }
 
 /* pages */
-function Welcome() {
-    return (
-        <div>
-            <h6>Welcome page</h6>
-            <h2><Link to={"/home"}>Go to Home</Link></h2>
-        </div>
-    );
-}
-function Home() {
-    const sections ={
-        sectionTitle:["Trending","Popular","Upcoming","NowPlaying"],
-        sectionToFetch:["getTrendingPage","getPopularPage","getUpcomingPage","getNowPlayingPage","getDetailsById"]
-    }
+/* home page */
+function HomePage() {
     return (
         <div>
             <header>
@@ -60,144 +55,111 @@ function Home() {
             </header>
             <main>
                 <h2>movies</h2>
-
-                <PageSection
-                    sectionTitle={sections.sectionTitle[0]}
-                    sectionToFetch={sections.sectionToFetch[0]}
-                    mediaType={"movie"}
-                    pageNumb={1}
-                />
             </main>
         </div>
     );
 }
-// function Movies() {
-//     const sections ={
-//         sectionTitle:["Trending","Popular","Upcoming","NowPlaying"],
-//         sectionToFetch:["getTrendingPage","getPopularPage","getUpcomingPage","getNowPlayingPage","getDetailsById"]
-//     }
-//     return (
-//         <div>
-//             <header>
-//                 <h2>Movies</h2>
-//                 <div className="carousel">Carousel</div>
-//             </header>
-//             <main>
-//                 <PageSection
-//                     sectionTitle={sections.sectionTitle[0]}
-//                     sectionToFetch={sections.sectionToFetch[0]}
-//                     mediaType={"movie"}
-//                     pageNumb={1}
-//                 />
-//                 <PageSection
-//                     sectionTitle={sections.sectionTitle[1]}
-//                     sectionToFetch={sections.sectionToFetch[1]}
-//                     mediaType={"movie"}
-//                     pageNumb={1}
-//                 />
-//                 <PageSection
-//                     sectionTitle={sections.sectionTitle[2]}
-//                     sectionToFetch={sections.sectionToFetch[2]}
-//                     mediaType={"movie"}
-//                     pageNumb={1}
-//                 />
-//                 <PageSection
-//                     sectionTitle={sections.sectionTitle[3]}
-//                     sectionToFetch={sections.sectionToFetch[3]}
-//                     mediaType={"movie"}
-//                     pageNumb={1}
-//                 />
-//             </main>
-//         </div>
-//     );
-// }
-// function TvShow() {
-//     const sections ={
-//         sectionTitle:["Trending","Popular","Upcoming","NowPlaying"],
-//         sectionToFetch:["getTrendingPage","getPopularPage","getUpcomingPage","getNowPlayingPage","getDetailsById"]
-//     }
-//     return (
-//         <div>
-//             <header>
-//                 <h2>Tv Show</h2>
-//                 <div className="carousel">Carousel</div>
-//             </header>
-//             <main>
-//                 <PageSection
-//                     sectionTitle={sections.sectionTitle[0]}
-//                     sectionToFetch={sections.sectionToFetch[0]}
-//                     mediaType={"tv"}
-//                     pageNumb={1}
-//                 />
-//                 <PageSection
-//                     sectionTitle={sections.sectionTitle[1]}
-//                     sectionToFetch={sections.sectionToFetch[1]}
-//                     mediaType={"tv"}
-//                     pageNumb={1}
-//                 />
-//             </main>
-//         </div>
-//     );
-// }
+/* movies page */
+function MoviesPage() {
+    return (
+        <div>
+            <header>
+                <h2>Movies</h2>
+                <div className="carousel">Carousel</div>
+            </header>
+            <main>
+
+            </main>
+        </div>
+    );
+}
+/* Tv SHow page */
+function TvShowPage() {
+    return (
+        <div>
+            <header>
+                <h2>Tv Show</h2>
+                <div className="carousel">Carousel</div>
+            </header>
+            <main>
+
+            </main>
+        </div>
+    );
+}
+/* Search page */
+function SearchPage() {
+    return (
+        <div>
+            <header>
+                <h2>Search</h2>
+                <div className="carousel">Search input</div>
+            </header>
+            <main>
+
+            </main>
+        </div>
+    );
+}
 
 
 
 
 // fetching section
 
-function PageSection (prop) {
-    const {sectionTitle, sectionToFetch, mediaType, pageNumb} = prop
+// function PageSection (prop) {
+//     const {sectionTitle, sectionToFetch, mediaType, pageNumb} = prop
 
-    const [apiData, setApiData] = useState()
-    useEffect(()=>{
-        TMDB[mediaType][sectionToFetch](pageNumb).then(data=>setApiData(data))
+//     const [apiData, setApiData] = useState()
+//     useEffect(()=>{
+//         TMDB[mediaType][sectionToFetch](pageNumb).then(data=>setApiData(data))
 
-    },[setApiData, mediaType, sectionToFetch, pageNumb])
-    console.log(apiData)
-    return (
-        <div>
-            <h4>{sectionTitle}</h4>
-            <div className="item-container">
-                    {/* {apiData?.results?.map((itemData,key)=>(
-                        <Item {...itemData} key={key} />
-                    ))} */}
-                        <Item {...apiData?.results[0]}/>
-            </div>
-        </div>
-    );
-}
+//     },[setApiData, mediaType, sectionToFetch, pageNumb])
+//     console.log(apiData)
+//     return (
+//         <div>
+//             <h4>{sectionTitle}</h4>
+//             <div className="item-container">
+//                     {/* {apiData?.results?.map((itemData,key)=>(
+//                         <Item {...itemData} key={key} />
+//                     ))} */}
+//                         <Item {...apiData?.results[0]}/>
+//             </div>
+//         </div>
+//     );
+// }
 
-function Item(prop) {
-    const { backdrop_path, first_air_date, name,title ,origin_country,vote_average} = prop;
-    return (
-        <div className='item'>
-            <div className='item-inner'>
-                <Link to={`/overview/${name}`}>
-                    <img
-                        src={`https://image.tmdb.org/t/p/w342${backdrop_path}`}
-                        alt=''
-                    />
-                </Link>
-                <div className='meta'>
-                    <h6>
-                        {title ? (
-                            <Link to={`/overview/${title}`}>{title}</Link>
-                        ) : (
-                            <Link to={`/overview/${name}`}>{name}</Link>
-                        )}
-                    </h6>
-                    <div>
-                        <span>{first_air_date?.replace("-", " ")}</span>
-                        <br />
-                        <span>Rating: {vote_average}</span>
-                        <br />
-                        <span>Country: {origin_country}</span>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
-}
+// function Item(prop) {
+//     const { backdrop_path, first_air_date, name,title ,origin_country,vote_average} = prop;
+//     return (
+//         <div className='item'>
+//             <div className='item-inner'>
+//                 <Link to={`/overview/${name}`}>
+//                     <img
+//                         src={`https://image.tmdb.org/t/p/w342${backdrop_path}`}
+//                         alt=''
+//                     />
+//                 </Link>
+//                 <div className='meta'>
+//                     <h6>
+//                         {title ? (
+//                             <Link to={`/overview/${title}`}>{title}</Link>
+//                         ) : (
+//                             <Link to={`/overview/${name}`}>{name}</Link>
+//                         )}
+//                     </h6>
+//                     <div>
+//                         <span>{first_air_date?.replace("-", " ")}</span>
+//                         <br />
+//                         <span>Rating: {vote_average}</span>
+//                         <br />
+//                         <span>Country: {origin_country}</span>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// }
 
 
 
