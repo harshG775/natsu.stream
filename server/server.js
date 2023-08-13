9791e2297297fe2dff3888b70757099c56f97dc9
Harsh <hgaur491@gmail.com>
2023-08-13 17:58:00 +0530

    adding script to run react build command and run server

--- server/server.js
+++ server/server.js
@@ -1,17 +1,18 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const PORT = process.env.PORT || 3030;
 
 
-// for serving react app after npm run build
-// app.use(express.static(path.join(__dirname, 'client/build')));
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname+'/client/build/index.html'));
-// });
-
-app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
+/* for serving react app after npm run build */
+app.use(express.static(path.join(__dirname, '../client/dist')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'../client/dist/index.html'));
 });
+
+// app.get("/api", (req, res) => {
+//     res.json({ message: "Hello from server!" });
+// });
 
 app.listen(PORT, () => {
     console.log(`server started at http://localhost:${PORT}`);
