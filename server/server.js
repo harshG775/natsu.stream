063af7b51627248cc043b84ef4bb0bfd266c56df
Harsh <hgaur491@gmail.com>
2023-08-13 19:11:52 +0530

    changing vite app build command to server/public

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.get("/api/trending/all", (req, res) => {
 
 // universal route
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
 app.listen(PORT, () => {
