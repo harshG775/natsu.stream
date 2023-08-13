06b69b7d76ade67899fb606e0820d2a1f026c7ef
Harsh <hgaur491@gmail.com>
2023-08-13 18:25:06 +0530

    /update

---
+++ server/modules/myFetch.js
@@ -0,0 +1,14 @@
+module.exports =  async (url) => {
+    try {
+        const response = await fetch(url);
+
+        if (!response.ok) {
+            throw new Error(`Request failed with status: ${response.status}`);
+        }
+
+        const jsonData = await response.json();
+        return jsonData;
+    } catch (error) {
+        throw new Error(`An error occurred while fetching data: ${error.message}`);
+    }
+};
--- server/server.js
+++ server/server.js
@@ -3,16 +3,22 @@ const app = express();
 const path = require("path");
 const PORT = process.env.PORT || 3030;
 
+const TMDB = require("./modules/fetching")
 
 /* for serving react app after npm run build */
 app.use(express.static(path.join(__dirname, '../client/dist')));
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname+'../client/dist/index.html'));
+
+app.get("/api/trending/all", (req, res) => {
+    TMDB.getAllTrendingPage().then(data=>{
+        res.json(data);
+    })
 });
 
-// app.get("/api", (req, res) => {
-//     res.json({ message: "Hello from server!" });
-// });
+
+// universal route
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`server started at http://localhost:${PORT}`);
