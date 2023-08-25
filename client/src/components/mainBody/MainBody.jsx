db8b74902a361409896444a3614279bbe1c59074
Harsh <hgaur491@gmail.com>
2023-08-25 11:26:40 +0530

    fixing theme changer

--- client/src/components/mainBody/MainBody.jsx
+++ client/src/components/mainBody/MainBody.jsx
@@ -2,7 +2,7 @@ import "./mainBody.css"
 export default function MainBody(prop) {
     return (
         <>
-            <div id="main-body">{prop.children}</div>
+            <div id="main-body" data="theme">{prop.children}</div>
             <div id="bottom-gap"></div>
         </>
     );
