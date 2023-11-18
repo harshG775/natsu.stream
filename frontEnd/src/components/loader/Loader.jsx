62b27d0bb2b2b19e0018f4a777ff41954f7417a0
Harsh <hgaur491@gmail.com>
2023-11-18 22:14:53 +0530

    adding loader for suspanse in main

---
+++ frontEnd/src/components/loader/Loader.jsx
@@ -0,0 +1,13 @@
+import "./loader.css"
+export default function Loader() {
+    return (
+		<div className="wrapper">
+            <div className="circle"></div>
+            <div className="circle"></div>
+            <div className="circle"></div>
+            <div className="shadow"></div>
+            <div className="shadow"></div>
+            <div className="shadow"></div>
+        </div>
+	);
+}
--- frontEnd/src/main.jsx
+++ frontEnd/src/main.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React ,{Suspense} from "react";
 import ReactDOM from "react-dom/client";
 import AppRoutes from "./AppRoutes.jsx";
 import "./styles/index.css";
 import "./styles/customClasses.css";
+import Loader from "./components/loader/Loader.jsx";
 
 import {QueryClient,QueryClientProvider,} from "@tanstack/react-query"
 const queryClient = new QueryClient()
@@ -11,15 +12,14 @@ import DBContextProvider from "./store/DBContextProvider";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-
+	<Suspense fallback={<Loader/>}>
 		<DBContextProvider>
 			<QueryClientProvider client={queryClient}>
-				<AppRoutes />
-				
-<ReactQueryDevtools initialIsOpen />
+
+				<AppRoutes />		<ReactQueryDevtools initialIsOpen />
 
 			</QueryClientProvider>
 		</DBContextProvider>
-
+	</Suspense>
 	</React.StrictMode>
 );
